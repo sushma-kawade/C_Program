ce33c9e5542dfca5e4e7e437039626a84de1ecf6
sushma-kawade <124495645+sushma-kawade@users.noreply.github.com>
2023-04-17 16:39:19 +0530

    Accept one character from user and convert case of that character

---
+++ program3.4.c
@@ -0,0 +1,24 @@
+//Accept one character from user and convert case of that charactor
+#include<stdio.h>
+int DisplayConvert(CValue)
+{
+    if()
+    {
+        printf("%d",);
+    }
+    else if()
+    {
+        printf("%d",);
+
+    }
+
+}
+int main()
+{
+    char cValue = '\0';
+    printf("Enter character\n");
+    scanf("%c",&cValue);
+    DisplayConvert(cValue);
+    return 0;
+}
+
