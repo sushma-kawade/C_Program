0c0df98a6ae49deff7c3dfb7b61cc78addcfdc5a
sushma-kawade <124495645+sushma-kawade@users.noreply.github.com>
2023-04-17 16:22:01 +0530

    program to divide Marvellous on five time

---
+++ program1.2.c
@@ -0,0 +1,32 @@
+//problem statments : program to print 5 time Marvellous on the screen..
+
+/* Algorithma : 
+START 
+display the five time mavervvous on the scrren
+End
+*/
+
+//function name : void Display
+// Description : display the five time Marvellous on the screen
+//Input : integer
+//output : string
+//Author : kawde sushma kisan
+//Date : 18/10/2022
+
+#include<stdio.h>
+void Display()
+{
+    int i = 0;
+    for(i = 1; i <= 5;i++)
+    {
+    printf("Marvellous..\n");
+
+    }
+    
+}
+
+int main()
+{
+    Display();
+    return 0;
+}
