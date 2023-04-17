d91e5c1ce3540e7b722502552474ca1f26d0a2b1
sushma-kawade <124495645+sushma-kawade@users.noreply.github.com>
2023-04-17 17:09:13 +0530

    Add files via upload

---
+++ program7.3.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+void Display(int iNo)
+{
+    int iCnt =0;
+    if(iNo < 0)
+    {
+       iNo = -iNo;
+    }
+    for(iCnt = iNo; iCnt >= 1; iCnt--)
+    {
+        
+        printf("%d\t",-iCnt);
+    }
+        printf("0\t");
+        
+    
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("%d\t",iCnt);
+    }
+
+}
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number\n");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
