31200d4ff800807d315d2d714c5d185f8802f56a
sushma-kawade <124495645+sushma-kawade@users.noreply.github.com>
2023-04-17 17:02:47 +0530

    Accept two no from user and check wheter no are equal or not

---
+++ program6.3.c
@@ -0,0 +1,38 @@
+//write a program which accept two number and check whether number are equal or not...
+/*Input:10 10
+output:equal
+
+Input :10 12
+output:Not equal
+
+Input:10 10
+output:not equal*/
+
+#include<stdio.h>
+typedef int BOOL;
+
+#define TRUE 1
+#define FALSE 0
+
+BOOL ChkEqual()
+{
+
+}
+int main()
+{
+int iValue1 = 0,iValue2 = 0;
+
+BOOL bRet = FALSE;
+printf("please enter two number");
+scanf("%d%d",&iValue1,&iValue2);
+bRet = (,);
+    if()
+    {
+    printf("Equal");
+    }
+    else
+    {
+    printf("");
+    }
+return 0;
+}
