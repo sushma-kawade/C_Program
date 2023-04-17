cd43fa70f2573bac7b06b41dc17ef063f1ef413b
sushma-kawade <124495645+sushma-kawade@users.noreply.github.com>
2023-04-17 16:40:50 +0530

    t character is vowel(a,e,i,o,u)

---
+++ program3.5.c
@@ -0,0 +1,25 @@
+//Accept character from user and check whether that charactor is vowel(a,e,i,o,u) or not......
+#include<stdio.h>
+typedef int ;
+#define 
+chkVowel(char)
+{
+    if()
+    {
+
+    }
+    else
+    {
+        return FALSE;
+
+    }
+}
+int main()
+{
+    char cValue = '\0';
+    BOOL bRet = FALSE;
+
+    printf("Enter character\n");
+    scanf("%c",&cValue);
+    bRet = 
+}
