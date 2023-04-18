2ddf98275a814aca69ae7b06254f98ddfd328a0d
sushma-kawade <124495645+sushma-kawade@users.noreply.github.com>
2023-04-18 16:55:21 +0530

    Accept N from user and return frequency of even no

---
+++ program15.1.c
@@ -0,0 +1,46 @@
+/*Accept N number from user and return frequency of even numbers.....
+Input:  N   :   6
+                Elements : 85   66  3   80  93  88  
+output          3          ....*/
+#include<stdio.h>
+#include<stdlib.h>
+
+int CountEven(int Arr[], int iLength)
+{
+    int iCnt = 0,iFrequency = 0;
+
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        if((Arr[iCnt] % 2) == 0)
+        {
+            iFrequency++;
+        }
+    }
+    return iFrequency;
+
+}
+int main()
+{
+    int iSize = 0, iRet = 0, iCnt = 0;
+    int *p = NULL;
+
+    printf("Enter number of elements\n");
+    scanf("%d",&iSize);
+
+    p = (int*)malloc(iSize*sizeof(int) * iSize);
+    
+    printf("Enter the elements : \n");
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        scanf("%d",&p[iCnt]);
+    }
+        
+        iRet = CountEven(p,iSize);
+
+        printf("Result is %d",iRet);
+        free(p);
+        return 0;
+
+
+}
