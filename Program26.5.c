05b08ea157adbb06b57b48c984b2910275ab7869
sushma-kawade <124495645+sushma-kawade@users.noreply.github.com>
2023-04-18 18:08:34 +0530

    write a program which accept string from user and count number of white space

---
+++ Program26.5.c
@@ -0,0 +1,42 @@
+/*write a program which accept string from user and count number of white space
+
+Input   marvellous
+output  0
+
+Input   Marvellous Infosystem
+output  1
+
+Input   marvellous multi os
+output  2...*/
+
+#include<stdio.h>
+
+int CountSpace(char *str)
+{
+    int iCnt = 0;
+
+    while(*str != '\0')
+    {
+        if(*str == ' ')
+        {
+            iCnt++;
+        }
+        str++;
+    }
+    return iCnt;
+}
+
+int main()
+{
+    char Arr[20];
+    int iRet = 0;
+
+    printf("Please enter string\n");
+    scanf("%[^'\n']s",Arr);
+
+    iRet = CountSpace(Arr);   // stelwrX(100);
+
+    printf("Number of white spaces are : %d\n",iRet);
+
+    return 0;
+}
