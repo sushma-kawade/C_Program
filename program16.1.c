0f65fba9779c702021668b01ef2d09c2eb2bf666
sushma-kawade <124495645+sushma-kawade@users.noreply.github.com>
2023-04-18 17:01:53 +0530

    Accept one another no from as NO ,check wheter NO is present or not

---
+++ program16.1.c
@@ -0,0 +1,60 @@
+/*Accept N number from user accept one another number as NO,Check whether NO is present or not.....
+Input:  N   :   6
+                Elements : 85   66  3    11   80  93   
+output          66 is present...
+
+Input:  N    :  6
+output:      :  12
+output:      :  Element :  85       11      3       11      111        */
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+typedef int BOOL;
+BOOL Check(int Arr[], int iLength,int iNo)
+{
+    int iCnt = 0;
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        if(Arr[iCnt] == iNo)
+        {
+            return -1;
+        }
+    
+    }
+    return iCnt;
+
+}
+int main()
+{
+    int iSize = 0,iSize1 = 0, iRet = 0, iCnt = 0;
+    int *p = NULL;
+    
+
+    printf("Enter number of elements\n");
+    scanf("%d",&iSize);
+
+    p = (int*)malloc(iSize*sizeof(int) * iSize);
+    
+    printf("Enter the elements : \n");
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        scanf("%d",&p[iCnt]);
+    }
+    printf("Enter another number : \n");
+    scanf("%d",&iSize1);
+        
+        iRet = Check(p,iSize,iSize1);
+
+        if(iRet == -1)
+        {
+            printf("NO is present");
+        }
+        else
+        {
+            printf("NO is not present");
+        }
+        free(p);
+        return 0;
+}
